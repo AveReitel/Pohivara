e6be25ce004429b120db2714631fc17b928a599c
AveReitel <lalalalalla@hotmail.com>
2016-03-15 14:12:31 +0200

    veeel uuendusi

--- Pohivara/src/Küsimus.java
+++ Pohivara/src/Küsimus.java
@@ -1,12 +1,13 @@
-public class Küsimused {
+public class Küsimus {
     private String definitsioon; // Definitsioon
     private String õige_vastus; // Õige vastus
     private String mängija_vastus; // Kasutaja pakutud vastus
     private boolean onÕige;
    
-    public void küsimused(String definitsioon, String answer) {
-        this.definitsioon = definitsioon;
-        this.õige_vastus = answer;     
+    public Küsimus(String definitsioon, String answer) {
+        this.definitsioon = definitsioon.replaceAll("(.{80} )", "$1\n");
+        this.õige_vastus = answer;   
+        
     }
  
     public String getU_answer() {
@@ -16,7 +17,7 @@ public class Küsimused {
     public void setU_answer(String mängija_vastus) {
         this.mängija_vastus = mängija_vastus.toLowerCase();
         // Kontrolli tulemust
-        if (mängija_vastus.toLowerCase().equals(õige_vastus.toLowerCase()))
+        if (mängija_vastus.equals(õige_vastus.toLowerCase()))
             onÕige = true;
         //onÕige = false;
     } 
@@ -28,5 +29,13 @@ public class Küsimused {
     } 
     public boolean onÕige() {
         return onÕige;
-    }   
+    }
+
+	@Override
+	public String toString() {
+		return "Küsimus [definitsioon=" + definitsioon + ", õige_vastus=" + õige_vastus + ", mängija_vastus="
+				+ mängija_vastus + ", onÕige=" + onÕige + "]";
+	}   
+    
+    
 }
--- Pohivara/src/Questionnaire.java
+++ Pohivara/src/Questionnaire.java
@@ -1,52 +1,67 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
  
 public class Questionnaire {
-    private List<Küsimused> küsimus; // Küsimuste list
+    private List<Küsimus> küsimused = new ArrayList<>(); // Küsimuste list
     private String mängija_nimi; // Mängija nimi
-    private Date alustusaeg; // Algusaeg
-    private Date lõppaeg; // Lõpuaeg
-    private List<Küsimused> küsitud ; // Küsitud küsimuste list, tulemuste jaoks
+    private List<Küsimus> küsitud = new ArrayList<>(); // Küsitud küsimuste list, tulemuste jaoks
     private int correctAnswers; // õigeid vastusied
-    private String kodeering = "UTF-8";
+    static String kodeering = "UTF-8";
    
-    public Questionnaire(String input,String kodeering, String name)
-    {
+    public Questionnaire(String input, String name) throws Exception {
+    	
         this.mängija_nimi = mängija_nimi;
-        parseInputFile(input);
-        alustusaeg = new Date(); // Praegune aeg saab algusajaks
+        faili_lugemine(input);
         alustaMängu();
+        
     }
    
-    private void parseInputFile(String input) {
-        // Siin loe fail sisse, tee oma failist Question tĆ¼Ć¼pi isendid
-        // Siia paned tsĆ¼kli, mis splitib sul need definitsioonid ja seletused Ć¤ra, ja nendest teed Qustion tĆ¼Ć¼pi isendid (konstruktorit vaata seal)
+    private void faili_lugemine(String input) throws Exception {
+    	File fail = new File(input);
+        Scanner sc = new Scanner(fail,kodeering);
+        while(sc.hasNextLine()) {
+            String rida = sc.nextLine();
+            String[] tükid = rida.split("\\#");
+            Küsimus küsimus = new Küsimus(tükid[1],tükid[0]);
+            küsimused.add(küsimus);
+        }
+        sc.close();
+        	
     }
    
     private void alustaMängu() {
-        // Testi pĆµhitsĆ¼kkel
-        while (küsimus.size() != 0)
-        {
-            int i =  0 + (int)(Math.random() * küsimus.size());
-            Küsimused k = küsimus.get(i);
-            Scanner scanner = new Scanner(System.in);
+    	
+    	Random counter = new Random();
+        int tsükli_pikkus = counter.nextInt(12)+5;
+        Scanner scanner = new Scanner(System.in);
+        for(int t = 0; t<tsükli_pikkus;t++){
+      
+            int i =  0 + (int)(Math.random() * küsimused.size());
+            Küsimus k = küsimused.get(i);
+            
             System.out.println(k.getDefinitsioon());
-            String mängija_vastus = scanner.next();
+            String mängija_vastus = scanner.nextLine();
             k.setU_answer(mängija_vastus);
-            küsimus.remove(i);
-            küsitud.add(k);          
+            küsimused.remove(i);
+            küsitud.add(k);      
+            
+            
         }
-        // LĆµpptulemus
-        for (Küsimused küsimus: küsitud) {
-            if (küsimus.onÕige())
-                System.out.println("✔: Tubli. Õige!"); // Muuda seda detailisemaks
-            else
-                System.out.println("✘: Eksisite!"); // Muda seda detailsemaks, mis oleks Ćµige olnud, mis tema vastas jne
-        }
-        System.out.println((new java.util.Date()).getTime() - alustusaeg.getTime()); //arvutan mängu kestuse
-        // Prindi vĆ¤lja testi tegemiseks kulunud aeg (finishtime - starttime)
+        scanner.close();
         
+        // LĆµpptulemus
+        for (Küsimus küsimus: küsitud) {
+            if (küsimus.onÕige())
+                System.out.println(küsimus.toString()+"✔: Tubli. Õige!"); // Muuda seda detailisemaks
+            else
+                System.out.println(küsimus.toString()+"✘: Eksisite!"); // Muda seda detailsemaks, mis oleks Ćµige olnud, mis tema vastas jne
+        }
+               
     }
    
  
