317c877a572e2bc9acaa7176a4495e7bf046bf39
Eliise <Eliise>
2016-03-15 15:21:27 +0200

    ...

--- Pohivara/src/Questionnaire.java
+++ Pohivara/src/Questionnaire.java
@@ -59,7 +59,7 @@ public class Questionnaire {
             if (küsimus.onÕige())
                 System.out.println(küsimus.toString()+"✔: Tubli. Õige!"); // Muuda seda detailisemaks
             else
-                System.out.println(küsimus.toString()+"✘: Eksisiteee!"); // Muda seda detailsemaks, mis oleks Ćµige olnud, mis tema vastas jne
+                System.out.println(küsimus.toString()+"✘: Eksisite!"); // Muda seda detailsemaks, mis oleks Ćµige olnud, mis tema vastas jne
         }
                
     }
